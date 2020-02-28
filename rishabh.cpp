40def63d88efa295187d79620db15df0fed8d278
Rishabh Sharma <57918940+nitw-rsa@users.noreply.github.com>
2020-02-28 11:44:51 +0530

    Added c++ competitive coding template

---
+++ rishabh.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<stack>
+#include<queue> //q.size() q.front() q.rear() q.pop() q.push()
+#include<unordered_map> // m.clear() m[]=""
+#include<map> //m.find() m.begin m .end() itr->first itr->second
+#include<algorithm>
+#include<boost/multiprecision/cpp_int.hpp>
+
+#define LL long long int
+#define ABS abs
+
+using namespace std;
+using boost::multiprecision::cpp_int;
+
+// a - '0'
+
+class Solution
+{
+    public:
+    LL test_cases;
+    
+    Solution()
+    {
+        
+    }
+    
+    void solve()
+    {
+        LL test_cases;
+        cin>>test_cases;
+		
+        while(test_cases--)
+        {
+			
+        }
+
+    }
+};
+
+int main()
+{
+	Solution s;
+	s.solve();
+	
+	return 0;
+}
